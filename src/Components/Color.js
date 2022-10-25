42f62943c55de31d64cecf880773628c89a43158
Parker Lum <parkerlum@berkeley.edu>
2022-10-24 23:45:35 -0700

    finished hw4

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 /* TODO: Translate the below class component to a functional component! */
-class Color extends React.Component {
+const Color  = (props) => {
+    /* TODO */
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
-    }
+    return (
+        <button onClick={() => props.onClick(props.color)}>Post {props.color}</button>
+        
+    );
 }
+
+
 
 export default Color;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import Color from './Color';
+import { useState } from 'react';
+import Block from './Block';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = () => {
+
+
+    function renderColor(color) {
+      
+      return <Color color={color} onClick={() => handleClick(color)}></Color> 
+    }
+
+    function handleClick(color) {
+   
+      const postCopy = post.slice();
+      const stylesObj = {
+        backgroundColor: color
+      }
+      postCopy.unshift(<Block color={color} stylesObj={stylesObj}></Block>)
+      
+      setPost(postCopy);
+    }
+
+
+    const [post, setPost] = useState([])
 
     return (
-      <div className="colorOptions">
-          {/* TODO */}
+      <div> 
+        <div className="colorOptions">
+          {renderColor("red")}
+          {renderColor("pink")}
+          {renderColor("blue")}
+          {renderColor("green")}
+        </div>
+        <div>
+          <p>{post}</p>
+          
+        </div>
       </div>
+
+
     );
 }
 
